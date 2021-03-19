32488cd8d84b3d338ab10902ab314fe27cf57e2a
kahyunKo <bufar1014@daum.net>
2021-03-19 21:07:01 +0900

    param-useBean acton tag practice commit

---
+++ src/ch04/com/dao/Person.java
@@ -0,0 +1,25 @@
+package ch04.com.dao;
+
+public class Person {
+	private int id = 201800002;
+	private String name = "°í°¡Çö";
+	
+	public Person() {
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
